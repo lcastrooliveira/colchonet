e94a4b240150079029e3a4053710199edfb37f13
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-04-10 13:23:25 -0300

    Finished chapter 6, use BCrypt for User Authentication, creat a rake taske to help migrate users' password, created application CSS and helper function for form errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates_presence_of :email, :full_name, :location, :password
-  validates_confirmation_of :password
   validates_length_of :bio, minimum: 30, allow_blank: false
   validates_uniqueness_of :email
 
   validate :email_format
+
+  has_secure_password
 
   private
   def email_format
